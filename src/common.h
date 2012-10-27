9198cfde5baab65c10729a38b5e968a1efccaa06
James Thompson <keiouu@gmail.com>
2012-10-27 10:45:25 +0100

    We are ready!

---
+++ src/common.h
@@ -0,0 +1,13 @@
+#ifndef COMMON_H
+#define COMMON_H 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#endif
