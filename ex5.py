1669919f86a3da7275595aabab05dfcdef3116d4
James Thompson <keiouu@gmail.com>
2012-11-24 19:17:21 +0000

    Remove legacy method

--- ex5.py
+++ ex5.py
@@ -29,25 +29,6 @@ def try_column(column):
 				stro += columns[len(columns) - 1 - j][i]
 
 			alt = not alt
-
-
-
-	# Re-order columns
-	o_columns = []
-	for i in range(0, len(columns)):
-		if i > (len(columns) - 1 - i):
-			break
-
-		if i == (len(columns) - 1 - i):
-			o_columns.append(columns[i])
-			break
-
-		o_columns.append(columns[i])
-		o_columns.append(columns[len(columns) - 1 - i])
-
-	stro = ""
-	for i in range(0, max(map(lambda a: len(a), o_columns))):
-		stro = stro + ''.join([x[i] for x in o_columns])
 	print stro
 
 try_column(4)
